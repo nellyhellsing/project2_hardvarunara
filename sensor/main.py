5f9a27881391563ce07e9a71ff4e49368bd7a068
Nelly Hellsing <nelly.hellsing@hotmail.com>
2023-09-28 10:48:31 +0200

    rename to main

--- sensor/main.py
+++ sensor/main.py
@@ -4,20 +4,19 @@ import ds18x20
 import time
 import json
 
-temp_config = open('config.json')
+temp_config = open("config.json")
 
 config = json.load(temp_config)
 
 
 # the device is on GPIO12
-dat = machine.Pin(config['pin'])
+dat = machine.Pin(config["pin"])
 
 # create the onewire objectd
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
 
-roms = ds.scan() 
+roms = ds.scan()
 id = machine.unique_id()
-
 
 id_hex = id.hex()
 
@@ -28,10 +27,8 @@ id_hex = id.hex()
 # loop 10 times and print all temperatures
 while True:
     ds.convert_temp()
-    time.sleep_ms(config['interval'])
+    time.sleep_ms(config["interval"])
     for rom in roms:
-        roms_2 = hex(int.from_bytes(rom, 'little'))
-        print(id_hex[2:], roms_2[2:], ds.read_temp(rom), end=' ')
+        roms_2 = hex(int.from_bytes(rom, "little"))
+        print(id_hex[2:], roms_2[2:], ds.read_temp(rom), end=" ")
         print()
-
-        
