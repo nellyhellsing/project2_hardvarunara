af7477b1351d33a3c439fdea06c9aec42f6bf6a5
Nelly Hellsing <nelly.hellsing@hotmail.com>
2023-10-11 14:40:42 +0200

    added comments

--- sensor/main.py
+++ sensor/main.py
@@ -4,24 +4,31 @@ import ds18x20
 import time
 import json
 
-temp_config = open("config.json")
 
+# Open and load the configuration file (config.json)
+temp_config = open("config.json")
 config = json.load(temp_config)
 
+# Initialize the one-wire data pin using the pin specified in the configuration
 dat = machine.Pin(config["pin"])
 
+# Initialize the DS18X20 temperature sensor on the one-wire
 temp_sensor = ds18x20.DS18X20(onewire.OneWire(dat))
 
+# Scan for DS18B20 temperature sensor devices on the one-wire
 roms = temp_sensor.scan()
 
+# Get the unique ID of the Raspberry Pi Pico in hexadecimal
 id = machine.unique_id()
-
 id_hex = id.hex()
 
+# Loop to continuously measure and report temperature
 while True:
     temp_sensor.convert_temp()
-    time.sleep_ms(config["interval"])
+    
+    # Delay for the specified interval before reading temperatures again
+    time.sleep_ms(config["interval"]) 
     for rom in roms:
         roms_2 = hex(int.from_bytes(rom, "little"))
         print(id_hex[2:], roms_2[2:], temp_sensor.read_temp(rom), end=" ")
-        print()
+        print()  # Print a new line to separate readings
